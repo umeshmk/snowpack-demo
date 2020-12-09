3aa40002274b9e5b594f506b40203c044516816e
umesh kadam <umeshk149@gmail.com>
2020-12-09 08:35:37 +0530

    snowpack basic demo working

---
+++ snowpack.config.js
@@ -0,0 +1,14 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/#configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+
+module.exports = {
+  // plugins: [],
+  // installOptions: {},
+  // devOptions: {},
+  // buildOptions: {},
+  // proxy: {},
+  // mount: {},
+  // alias: {},
+};
